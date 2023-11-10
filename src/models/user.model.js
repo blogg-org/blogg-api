0d50ed3a8228e621d3b70cd00d94735b7403c6ea
RameshNeupane <neupaneramesh555@gmail.com>
2023-11-10 20:10:37 +0545

    add db middlewares for signup and signin

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import pkg from "bcryptjs";
+const { genSalt, hash, compare } = pkg;
 
 const userSchema = new mongoose.Schema(
 	{
@@ -20,5 +22,25 @@ const userSchema = new mongoose.Schema(
 	},
 	{ timestamps: true }
 );
+
+// hash password before user is saved to database
+userSchema.pre("save", async function (next) {
+	const user = this;
+	try {
+		const salt = await genSalt(10);
+		const hashedPassword = await hash(user.password, salt);
+		user.password = hashedPassword;
+		next();
+	} catch (error) {
+		next(error);
+	}
+});
+
+// compare password for signin
+userSchema.method("comparePassword", async function comparePassword(password) {
+	const user = this;
+	const isMatched = await compare(password, user.password);
+	return isMatched;
+});
 
 export const User = mongoose.model("User", userSchema);
