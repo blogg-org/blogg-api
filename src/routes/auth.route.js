0b3b955ffd2adba1fda8d66d49babaa1befdd8c4
RameshNeupane <neupaneramesh555@gmail.com>
2023-11-23 14:45:57 +0545

    update auth route and controller

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import limiter from "../middlewares/limiter.middleware.js";
-import { handleSignin, handleSignup } from "../controllers/auth.controller.js";
+import {
+	handleRefresh,
+	handleSignin,
+	handleSignout,
+	handleSignup,
+} from "../controllers/auth.controller.js";
 
+/*
+==============================================
+AUTH ROUTER
+==============================================
+ */
 const authRouter = Router();
 
 authRouter.route("/signup").post(limiter, handleSignup);
 authRouter.route("/signin").post(limiter, handleSignin);
+authRouter.route("/refresh").get(handleRefresh);
+authRouter.route("/signout").post(handleSignout);
 
 export default authRouter;
