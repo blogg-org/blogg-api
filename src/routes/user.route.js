238bcd5cd83f1b0fde08df4d1d27199afbc6a098
RameshNeupane <neupaneramesh555@gmail.com>
2023-11-23 14:46:14 +0545

    update user route and controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,18 @@
 import express from "express";
 import {
 	handleGetAllUsers,
-	handleGetUserById,
+	handleGetCurrentUser,
 } from "../controllers/user.controller.js";
+import checkAuth from "../middlewares/checkAuth.middleware.js";
 
+/*
+==============================================
+USER ROUTER
+==============================================
+ */
 const userRouter = express.Router();
+
 userRouter.route("/all-users").get(handleGetAllUsers);
-userRouter.route("/:userId").get(handleGetUserById);
+userRouter.route("/current-user").get(checkAuth, handleGetCurrentUser);
 
 export default userRouter;
